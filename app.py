9054425f7ebec5cb318c1a8f707b09b7d7bed818
Circle Developer Indonesia <circledeveloperid@gmail.com>
2021-09-19 12:12:55 +0700

    Repatch

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import genshinstats as gs
 from flask import Flask, jsonify, request
 from flask import render_template
+import json
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-gs.set_cookie(ltuid=119480035, ltoken="cnF7TiZqHAAvYqgCBoSPx5EjwezOh1ZHoqSHf7dT")
+data = open('cookie.json',"r")
+data_cookie = json.load(data)
+cookie_used = 0 #index
+gs.set_cookie(ltuid=data_cookie[0]["ltuid"], ltoken=data_cookie[0]["ltoken"])
 #uid = 710785423
 app = Flask(__name__)
  
@@ -28,7 +32,7 @@ def api_user():
 		data = gs.get_user_stats(uid)
 		return jsonify(data)
 	except Exception as e:
-		return jsonify({"error":str(e)})
+		return smart_handle_limit(e)
 
 @app.route("/spa")
 def api_spa():
@@ -40,7 +44,7 @@ def api_spa():
 		data = gs.get_spiral_abyss(uid)
 		return jsonify(data)
 	except Exception as e:
-		return jsonify({"error":str(e)})
+		return smart_handle_limit(e)
 
 @app.route("/traveler")
 def api_traveler():
@@ -52,7 +56,20 @@ def api_traveler():
 		data = gs.get_all_user_data(uid)
 		return jsonify(data)
 	except Exception as e:
-		return jsonify({"error":str(e)})
+		return smart_handle_limit(e)
  
+def smart_handle_limit(e):
+	global cookie_used, data_cookie
+	if(str(e) == "Cannnot get data for more than 30 accounts per day."):
+		if(cookie_used < len(data_cookie)-1):
+			gs.set_cookie(ltuid=data_cookie[cookie_used+1]["ltuid"], ltoken=data_cookie[cookie_used+1]["ltoken"])
+			cookie_used+=1
+			return jsonify({"success":str("COOKIE berhasil direfresh, silahkan coba lagi!")})
+		else:
+			gs.set_cookie(ltuid=data_cookie[0]["ltuid"], ltoken=data_cookie[0]["ltoken"])
+			cookie_used = 0;
+	else:
+		return jsonify({"error":str(e)})
+
 if __name__ == "__main__":
     app.run()
